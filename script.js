2a4c4896eccf57ae71b2185d582d846371437686
Munim <munimsyed5126gmail.com>
2022-08-05 14:16:18 -0700

    Title changes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let titleInterval = document.getElementById("title");
+let title = document.getElementById("title");
 
-setInterval(function() {
-    titleInterval
-}, 1000)
+const word = "Munim Syed Presents The Lightning Spirit";
+
+let time;
+
+let l = 0;
+
+function titleInterval() {
+    console.log(l);
+    
+    if(l >= word.length - 1) {
+        clearInterval(time);
+    }
+    title.innerHTML += word[l];
+    l++;
+}
+time = setInterval(titleInterval, 150);
